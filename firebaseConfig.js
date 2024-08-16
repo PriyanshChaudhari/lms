db0469b6eade5ebd5f74b87f4b4b3ecd77508ee2
KrishKheni <khenikrish08@gmail.com>
2024-08-16 21:24:48 +0530

    Update Firebase configuration.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBhD5CZjcG1Fbtr7BraekTKiyyx_oEBtto",
-    authDomain: "lmsystem-2afd2.firebaseapp.com",
-    projectId: "lmsystem-2afd2",
-    storageBucket: "lmsystem-2afd2.appspot.com",
-    messagingSenderId: "528406607075",
-    appId: "1:528406607075:web:e5f14a7b76b419db2541d6",
-    measurementId: "G-NLK61D1FE1"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
