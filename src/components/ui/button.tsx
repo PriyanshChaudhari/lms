317c50ee3964cb9b4409a9ae5444cce356596fd6
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-09-01 15:48:08 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,8 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background rounded-[0.3rem] fond-semibold duration-1000 shadow-3xl hover:bg-slate-300 hover:text-black",
+          "bg-background rounded-[0.3rem] fond-semibold duration-1000 shadow-3xl hover:bg-slate-300 hover:text-black",
+        //border border-input
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
