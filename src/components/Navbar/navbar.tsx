9c838c9f7f648f15e28e19629456c33ae15f8786
PATEL YATHARTH <114477915+y4th4rthh@users.noreply.github.com>
2024-09-07 01:47:48 +0530

    Sidebar designed for admin and student pages.

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -127,10 +127,10 @@ const Navbar: React.FC = () => {
   }
 
   return (
-    <nav className="dark:bg-gray-800 p-4 bg-gray-100 text-black dark:text-white w-full z-10 top-0 font-rubik" style={{ cursor: 'default' }}>
+    <nav className="dark:bg-gray-800 p-4 bg-gray-100 text-black dark:text-white w-full z-10 top-0 sticky font-rubik" style={{ cursor: 'default' }}>
       <div className="mx-auto text-center flex w-5/6 justify-between font-bold text-black dark:text-white">
         <div className="text-3xl space-x-1 flex items-center sm:text-2xl font-extrabold">
-          Moodle
+          LMS
         </div>
 
         <div className="hidden sm:flex lg:space-x-10 items-center gap-6 text-md font-semibold">
