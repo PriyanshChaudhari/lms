ad7a2b144149689e45f14c93b7b583a123850eac
Priyansh Chaudhari <priyanshchaudhari3@gmail.com>
2024-12-17 17:26:58 +0530

    Minor Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/Theme/theme-provider";
+import "./globals.css";
+import { ModeToggle } from "../components/Theme/toggleTheme";
+import Navbar from "@/components/Navbar/navbar";
 import Head from 'next/head';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "MSU LMS",
+    description: "Learning Management System for MSU Baroda",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
     return (
-        <>
+        <html lang="en">
             <Head>
                 <title>MSU LMS - Your Learning Management System</title>
                 <meta name="description" content="Learning Management System for MSU Baroda" />
-                <meta name="keywords" content="LMS, Learning Management System, Education, MSU" />
+                <meta name="keywords" content="MSU LMS, msu-lms,LMS MSU, LMS MSU Baroda, LMS for MSU Baroda, MSU Baroda, msu-lms netlify, msu lms netlify, LMS, Learning Management System, Education, MSU" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="canonical" href="https://msu-lms.netlify.app" />
                 <meta property="og:title" content="MSU LMS" />
@@ -29,19 +45,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                     })}
                 </script>
             </Head>
-            <header>
-                <nav>
-                    {/* Add your navigation items here */}
-                </nav>
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                <p>&copy; {new Date().getFullYear()} MSU LMS. All rights reserved.</p>
-            </footer>
-        </>
+            <body className={inter.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {/* <ModeToggle /> */}
+                    <Navbar />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
     );
-};
-
-export default Layout;
+}
